77508b27b01c0048acb3ba831c7c19b5c3d082b1
Adel <adelnmo2003@gmail.com>
2024-09-28 22:04:46 +0330

    flat flatMap

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 const numbers = [-5, -1, -8, -2];
 
 const result = numbers.reduce((acc, curValue) => {
@@ -22,3 +23,22 @@ console.log(result);
 
 // STEP 4
 // return finalResult
+*/
+
+// const arr = [[1, 2], [[3], 4], 5];
+// console.log(arr);
+
+// console.log(arr.flat());
+// //          [1, 2, [3], 4, 5]
+
+// console.log(arr.flat(2));
+// //          [1, 2, [3], 4, 5]
+// //          [1, 2, 3, 4, 5]
+
+const arr = [1, 2, 3, 4];
+
+const mapResult = arr.map((value) => [value]);
+const flatMapResult = arr.flatMap((value) => [value]);
+
+console.log(mapResult);
+console.log(flatMapResult);
